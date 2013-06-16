53255ff5d2972b82dffb7e9a1be6fde1fa8c0e0c
Anirudha Bose <ani@linux-evnl.site>
2013-06-16 12:26:33 +0530

    Updated repository to reflect changes Removed class Visual and deleted visua.html

--- main.py
+++ main.py
@@ -5,18 +5,11 @@ import jinja2
 import os
 
 jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
-
-class MainHandler(webapp2.RequestHandler):
+		
+class CLI(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/html'
-        template = jinja_environment.get_template('index.html')
-        self.response.out.write(template.render())
-
-class Visual(webapp2.RequestHandler):
-    def get(self):
-        self.response.headers['Content-Type'] = 'text/html'
-        template = jinja_environment.get_template('visual.html')
+        template = jinja_environment.get_template('cli.html')
         self.response.out.write(template.render())
         
-app = webapp2.WSGIApplication([('/', MainHandler),
-							   ('/visual', Visual)],debug=True)
+app = webapp2.WSGIApplication([('/', CLI)],debug=True)
